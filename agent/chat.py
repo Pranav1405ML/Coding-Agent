dcf2ed9a3de9b6ff3cc06b19b169fb26a71dc6ac
Pranav Ganesh <pranavganesh1405@gmail.com>
2026-06-24 13:16:07 +0530

    Split exception handling in agent loop for precise error attribution

--- agent/chat.py
+++ agent/chat.py
@@ -13,8 +13,9 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 def send_message(history: list, user_message: str) -> tuple:
     temp_history = history + [{"role": "user", "parts": [{"text": user_message}]}]
 
-    try:
-        while True:
+
+    while True:
+        try:
             response = client.models.generate_content(
                 model="gemini-3.1-flash-lite",
                 contents=temp_history,
@@ -22,21 +23,29 @@ def send_message(history: list, user_message: str) -> tuple:
                     tools=[all_tools]
                 )
             )
+        # You're capturing that error object into the variable e, so you can inspect it, log it, or show it to the user — instead of just knowing "something broke" with zero detail.
+        except Exception as e:
+            return f"[Error: Could not get a response. {e}]", history
 
-            function_call = response.candidates[0].content.parts[0].function_call
+        function_call = response.candidates[0].content.parts[0].function_call
 
-            if function_call:
-                temp_history.append(response.candidates[0].content)
+        if function_call:
+            temp_history.append(response.candidates[0].content)
+            try:
                 result = TOOL_REGISTRY[function_call.name]["function"](**function_call.args)  # KeyError will be caught in exception as e
-                temp_history.append({"role": "function", "parts": [{"function_response": {"name": function_call.name, "response": {"result": result}}}]})
+            except KeyError as e:
+                return f"[Error: Key does not exist in Tool Registry. {e}]", history
+            except TypeError as e:
+                return f"[Error: Invalid number of parameters. {e}]", history
+            except Exception as e:
+                return f"[Error: Something went wrong, {e}]", history
 
-            else:
-                temp_history.append({"role": "model", "parts": [{"text": response.text}]})
-                history = temp_history
-                return response.text, history
+            temp_history.append({"role": "function", "parts": [{"function_response": {"name": function_call.name, "response": {"result": result}}}]})
 
-    # You're capturing that error object into the variable e, so you can inspect it, log it, or show it to the user — instead of just knowing "something broke" with zero detail.
-    except Exception as e:
-        return f"[Error: Could not get a response. {e}]", history
+        else:
+            temp_history.append({"role": "model", "parts": [{"text": response.text}]})
+            history = temp_history
+            return response.text, history
+
 
 
